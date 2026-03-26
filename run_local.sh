4662de99f7385f81ba38b2d40b5b053a47c613c1
dennis <vuhuy999999@gmail.com>
2026-03-26 12:13:20 +0800

    updated codebase

--- run_local.sh
+++ run_local.sh
@@ -1,9 +1,16 @@
+# run this code in window wont get any error
+
 python3 -m venv env
 
-source env/bin/activate
+./env/bin/activate
+
+# run in Linux just change the way of activating virtual environment
+
+# source ./env/bin/activate
 
 pip install --upgrade pip
 
 pip install -r requirements.txt
 
 python app.py
+
