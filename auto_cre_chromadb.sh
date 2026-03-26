0810439b875d77136d7a4b923181e2ad83ce5efb
DennisWu <114161383+DennisWu0@users.noreply.github.com>
2026-03-26 17:29:15 +0800

    Update auto_cre_chromadb.sh

--- auto_cre_chromadb.sh
+++ auto_cre_chromadb.sh
@@ -1,7 +1,14 @@
 # Step1: reformat and then place to this directory 
 # /home/dennis/workspace/adaptive_chunksize/dataset
+# and make the data's format like below 
+#{
+#  "document": [],
+#  "question": [],
+#  "answer": [],
+#  "ori_doc_title": []
+#}
 
 # Step2: adjust .env file
 
-
+# Step3: run
 python chroma_project/database/main.py 
