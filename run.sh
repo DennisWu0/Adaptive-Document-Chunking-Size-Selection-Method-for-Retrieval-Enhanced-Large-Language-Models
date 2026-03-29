631d0865a325252152979a16458d24ec8edff3cc
Dennis Wu <vuhuy999999@gmai.com>
2026-03-29 08:23:23 +0800

    update the Dockerfile

--- run.sh
+++ run.sh
@@ -1,15 +1,6 @@
 # Build the Docker image
 docker build -t adaptive-chunks .
 
-
-# docker run --gpus all -p 5007:5007 \
-#     -v $(pwd)/.env:/app/.env \
-#     -v /home/dennis/workspace/adaptive_chunksize/chromadb/narrativeqa_chromadb/:/app/chromadb/narrativeqa_chromadb \
-#     -v /home/dennis/workspace/adaptive_chunksize/chromadb/natural_question_chromadb/:/app/chromadb/natural_question_chromadb \
-#     -v /home/dennis/workspace/adaptive_chunksize/chromadb/quac_chromadb/:/app/chromadb/quac_chromadb \
-#     -v /home/dennis/workspace/adaptive_chunksize/chromadb/triviaqa_chromadb/:/app/chromadb/triviaqa_chromadb \
-#     adaptive-chunks
-
 # Run the container
 docker run --gpus all -p 5007:5007 \
     -v $(pwd)/.env:/app/.env \
